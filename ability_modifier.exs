529974138d51acfe4ddd22e5140ff3f4a999e34f
Marvin <marvin.zwolsman@gmail.com>
2018-12-05 15:07:14 +0100

    Guard on case (positive number only)

--- ability_modifier.exs
+++ ability_modifier.exs
@@ -2,12 +2,12 @@ user_input = IO.gets("Write your ability score:\n")
 
 result =
   case Integer.parse(user_input) do
-    :error ->
-      "Invalid ability score: #{user_input}"
-
-    {ability_score, _} ->
+    {ability_score, _} when ability_score >= 0 ->
       ability_modifier = (ability_score - 10) / 2
       "Your ability modifier is #{ability_modifier}"
+
+    _ ->
+      "Invalid ability score: #{user_input}"
   end
 
 IO.puts(result)
