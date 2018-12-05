a602039d9745005d6bf3880bd0460a3971e71626
Marvin <marvin.zwolsman@gmail.com>
2018-12-05 15:05:45 +0100

    One time use of IO.puts, case return value

--- ability_modifier.exs
+++ ability_modifier.exs
@@ -1,10 +1,13 @@
 user_input = IO.gets("Write your ability score:\n")
 
-case Integer.parse(user_input) do
-  :error ->
-    IO.puts("Invalid ability score: #{user_input}")
+result =
+  case Integer.parse(user_input) do
+    :error ->
+      "Invalid ability score: #{user_input}"
 
-  {ability_score, _} ->
-    ability_modifier = (ability_score - 10) / 2
-    IO.puts("Your ability modifier is #{ability_modifier}")
-end
+    {ability_score, _} ->
+      ability_modifier = (ability_score - 10) / 2
+      "Your ability modifier is #{ability_modifier}"
+  end
+
+IO.puts(result)
