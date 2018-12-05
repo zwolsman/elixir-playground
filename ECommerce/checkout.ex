d7466461ce3d0cd15d4c2949000910fbac05817d
Marvin <marvin.zwolsman@gmail.com>
2018-12-05 14:58:38 +0100

    Own defguard definition

--- ECommerce/checkout.ex
+++ ECommerce/checkout.ex
@@ -1,3 +1,12 @@
 defmodule ECommerce.Checkout do
-  def total_cost(price, tax_rate) when price >= 0 and tax_rate >= 0, do: price * (tax_rate + 1)
+  defguard is_rate(value) when is_float(value) and value >= 0 and value <= 1
+  defguard is_cents(value) when is_integer(value) and value >= 0
+
+  def total_cost(price, tax_rate) when is_cents(price) and is_rate(tax_rate) do
+    price + tax_cost(price, tax_rate)
+  end
+
+  def tax_cost(price, tax_rate) when is_cents(price) and is_rate(tax_rate) do
+    price * tax_rate
+  end
 end
