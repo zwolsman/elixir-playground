3f7357282ebc169318d0dd81d9618450cfad440b
Marvin <marvin.zwolsman@gmail.com>
2018-12-05 19:47:36 +0100

    Tax calculation

--- practice-chapter3.exs
+++ practice-chapter3.exs
@@ -9,3 +9,15 @@ input = %{strength: 5, dexterity: 2, intelligence: 3}
 
 IO.inspect(input)
 IO.puts("Points spent: #{RPGs.points_spent(input)}")
+
+#  Create a function that returns Tic-Tac-Toe game winners. You can repre- sent the board with a tuple of nine elements, where each group of three items is a row. The return of the function should be a tuple. When we have a winner, the first element should be the atom :winner, and the second should be the player. When we have no winner, the tuple should contain one item that is the atom :no_winner.
+# TODO
+
+# Create a function that calculates income tax following these rules: a salary equal or below $2,000 pays no tax; below or equal to $3,000 pays 5%; below or equal to $6,000 pays 10%; everything higher than $6,000 pays 15%.
+
+defmodule Income do
+  def tax(salary) when salary <= 2000, do: 0
+  def tax(salary) when salary <= 3000, do: 0.05
+  def tax(salary) when salary <= 6000, do: 0.1
+  def tax(salary), do: 0.15
+end
